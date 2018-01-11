15e51d350f2f5c48657d2c7f93d9747528fb66a7
Brian Thompson <brian.thompson@iovation.com>
2018-01-11 15:58:31 -0800

    work on primarykey

--- trace.py
+++ trace.py
@@ -10,14 +10,14 @@ authenticate("localhost:7474", "neo4j", "P0rsch3")
 # connect to authenticated graph database
 graph = Graph("http://localhost:7474/db/data/")
 
-#graph.schema.create_uniqueness_constraint("host","ip")
+# graph.schema.create_uniqueness_constraint("host","ip")
 
-class Host(GraphObject):
-    __primarykey__ = "ip"
-    ip = Property()
-    last = Property()
-    ttl = Property()
-    path = RelatedTo("Host")
+#class Host(GraphObject):
+#    __primarykey__ = "ip"
+#    ip = Property()
+#    last = Property()
+#    ttl = Property()
+#    path = RelatedTo("Host")
 
 
 dublin = dublintraceroute.DublinTraceroute('8.8.8.8', npaths=1)
@@ -36,24 +36,30 @@ for flow in d['flows'].keys():
             print(d['flows'][flow][hop]['received']['ip']['src'])
             print(d['flows'][flow][hop]['sent']['ip']['ttl'])
             print(d['flows'][flow][hop]['received']['timestamp'])
-            a = Host()
-            a.ip = d['flows'][flow][hop]['received']['ip']['src']
-            a.ttl = d['flows'][flow][hop]['sent']['ip']['ttl']
-            a.last = d['flows'][flow][hop]['received']['timestamp']
+            ip = d['flows'][flow][hop]['received']['ip']['src']
+            ttl = d['flows'][flow][hop]['sent']['ip']['ttl']
+            last = d['flows'][flow][hop]['received']['timestamp']
+            a = Node("Host", ip=ip, ttl=ttl, last=last)
+            a.__primarykey__ = a[ip]
+
             tx.merge(a)
         if d['flows'][flow][hop]['is_last'] is False:
             if d['flows'][flow][hop]['received'] is not None:
                 if d['flows'][flow][(hop+1)]['received'] is not None:
-                    a.path = d['flows'][flow][(hop+1)]['received']['ip']['src']
-                    print(a.ip)
-                    print(a.path)
-                    tx.merge(a)
+                    ip = d['flows'][flow][(hop+1)]['received']['ip']['src']
+                    b = Node("Host", ip=ip)
+                    b.__primarykey__ = b[ip]
+#                    a.path = d['flows'][flow][(hop+1)]['received']['ip']['src']
+                    print(a)
+                    print(b)
+#                    tx.merge(a)
 #                    b.path = a
 #                    graph.push(b)
 #                    tx.create(ba)
 
-#            ab = Relationship(a, "KNOWS", b)
-#            tx.create(ab)
+            ab = Relationship(a, "PATH", b)
+            print(ab)
+            tx.merge(ab)
 tx.commit()
 
 #    print(results['flows'][flow][2])
